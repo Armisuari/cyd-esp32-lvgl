f68c30b11016cab83287a33f70f4eba6f1bf96a3
Arief Nur Misuari <ariefnurmisuari@gmail.com>
2026-01-24 14:20:13 +0700

    feat: Add firmware versioning parameters in `param.h` and update changelog to reflect release version.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.0.1-develop] - 2026-01-24
+## [0.0.1-release] - 2026-01-24
 
 ### Added
 - **Firmware Versioning**: Introduced a versioning system in `include/param.h` (Major, Minor, Patch, Branch).
--- include/param.h
+++ include/param.h
@@ -4,7 +4,7 @@
 #define FIRMWARE_VERSION_MAJOR 0
 #define FIRMWARE_VERSION_MINOR 0
 #define FIRMWARE_VERSION_PATCH 1
-#define FIRMWARE_VERSION_BRANCH "develop" // Branch number (0 for main branch, 1 for dev branch, etc.)
+#define FIRMWARE_VERSION_BRANCH "release" // Branch number (0 for main branch, 1 for dev branch, etc.)
 
 #define FIRMWARE_VERSION {FIRMWARE_VERSION_MAJOR, FIRMWARE_VERSION_MINOR, FIRMWARE_VERSION_PATCH} // Firmware version array
 
